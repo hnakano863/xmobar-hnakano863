4a06ef8ec19907a2e1bd98a05b685b8fd02b67d8
hnakano863 <notchi863@gmail.com>
2021-09-24 21:07:30 +0900

    簡単なところまでいったん作成

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,45 @@
 module Main where
 
-import Xmobar (xmobar, defaultConfig)
+import Xmobar
 
 main :: IO ()
-main = xmobar defaultConfig
+main = xmobar myConfig
+
+defaultFont :: String
+defaultFont = "xft:Iosevka Nerd Font-12"
+
+myAdditionalFonts :: [String]
+myAdditionalFonts =
+  [ "xft:feather-14"
+  , "xft:Iosevka Nerd Font-17"
+  ]
+
+myConfig :: Config
+myConfig
+  = defaultConfig
+  { overrideRedirect = False
+  , font = defaultFont
+  , additionalFonts = myAdditionalFonts
+  , bgColor = "#FFFFFF"
+  , fgColor = "#F8F8F2"
+  , alpha = 255
+  , position = TopSize L 100 26
+  , sepChar = "%"
+  , alignSep = "}{"
+  , commands = myCommands
+  , template = myTemplate
+  }
+
+myTemplate :: String
+myTemplate = "} %date% {"
+
+myCommands :: [Runnable]
+myCommands =
+  [ myDate
+  ]
+
+myDate :: Runnable
+myDate = Run $ Date dateModuleFormat "date" 150
+
+dateModuleFormat :: String
+dateModuleFormat = "<fn=1>\xe939</fn> %H:%M"
